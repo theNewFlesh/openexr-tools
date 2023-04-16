7c2818d1a46e252cb46aea1253e851f148059699
Alex Braun <alexander.g.braun@gmail.com>
2023-04-16 16:16:29 +0000

    added from_exr_code tests

--- python/openexr_tools/enum_test.py
+++ python/openexr_tools/enum_test.py
@@ -15,3 +15,25 @@ class ImageCodecTests(unittest.TestCase):
   string: pxr24
 exr_code: 5'''[1:]
         self.assertEqual(result, expected)
+
+    def test_from_exr_code(self):
+        self.assertEqual(ImageCodec.from_exr_code(0), ImageCodec.UNCOMPRESSED)
+        self.assertEqual(ImageCodec.from_exr_code(1), ImageCodec.RLE)
+        self.assertEqual(ImageCodec.from_exr_code(2), ImageCodec.ZIPS)
+        self.assertEqual(ImageCodec.from_exr_code(3), ImageCodec.ZIP)
+        self.assertEqual(ImageCodec.from_exr_code(4), ImageCodec.PIZ)
+        self.assertEqual(ImageCodec.from_exr_code(5), ImageCodec.PXR24)
+        self.assertEqual(ImageCodec.from_exr_code(6), ImageCodec.B44)
+        self.assertEqual(ImageCodec.from_exr_code(7), ImageCodec.B44A)
+        self.assertEqual(ImageCodec.from_exr_code(8), ImageCodec.DWAA)
+        self.assertEqual(ImageCodec.from_exr_code(9), ImageCodec.DWAB)
+
+    def test_from_exr_code_errors(self):
+        expected = 'Value given is not an integer. bar !=.*int'
+        with self.assertRaisesRegexp(EnforceError, expected):
+            ImageCodec.from_exr_code('bar')
+
+        expected = 'EXR code 99 has no legal ImageCodec type. '
+        expected += 'Legal EXR codes: .*'
+        with self.assertRaisesRegexp(EnforceError, expected):
+            ImageCodec.from_exr_code(99)
