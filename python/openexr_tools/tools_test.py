b9ee5862c7a8da3a4584bf60c620937ffe11494d
Alex Braun <alexander.g.braun@gmail.com>
2023-04-16 16:50:45 +0000

    more tools tests

--- python/openexr_tools/tools_test.py
+++ python/openexr_tools/tools_test.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
@@ -7,6 +8,7 @@ import OpenEXR as openexr
 import skimage.io
 import unittest
 
+from openexr_tools.enum import ImageCodec
 import openexr_tools.tools as tools
 # ------------------------------------------------------------------------------
 
@@ -66,3 +68,77 @@ class ToolsTests(unittest.TestCase):
             self.assertEqual(image.shape[2], len(metadata['channels']))
             self.assertEqual(metadata['channels'], list('rgba'))
             self.assertEqual(metadata['num_channels'], 4)
+
+    def test_read_exr_channels(self):
+        with TemporaryDirectory() as root:
+            expected = sorted(['foo', 'bar', 'baz'])
+            src = self.write_exr(root, np.float32, channels=expected)
+            image, metadata = tools.read_exr(src)
+            self.assertEqual(metadata['channels'], expected)
+
+            channels = ['foo', 'bar', 'baz'] + list('rgba')
+            src = self.write_exr(root, np.float32, channels=channels)
+            image, metadata = tools.read_exr(src)
+            expected = list('rgba') + sorted(['foo', 'bar', 'baz'])
+            self.assertEqual(metadata['channels'], expected)
+
+    def test_clean_exr_metadadata_channels(self):
+        image = np.zeros((10, 10), dtype=np.float32)
+        metadata = {}
+        result = tools.clean_exr_metadadata(image, metadata)
+        self.assertEqual(result['channels'], ['l'])
+
+        image = np.zeros((10, 10, 7), dtype=np.float32)
+        metadata = {}
+        result = tools.clean_exr_metadadata(image, metadata)
+        expected = [f'aux_{i:04d}' for i in range(7)]
+        self.assertEqual(result['channels'], expected)
+
+        channels = list('rgba') + ['foo']
+        metadata = dict(channels=channels)
+        result = tools.clean_exr_metadadata(image, metadata)
+        expected = channels
+        expected += [f'aux_{i:04d}' for i in range(2)]
+        self.assertEqual(result['channels'], expected)
+
+    def test_clean_exr_metadadata_forbidden(self):
+        image = np.zeros((10, 10), dtype=np.float32)
+        forbidden = dict(
+            compression='foo',
+            dataWindow='foo',
+            displayWindow='foo',
+            lineOrder='foo',
+            pixelAspectRatio='foo',
+            screenWindowCenter='foo',
+            screenWindowWidth='foo',
+        )
+        metadata = dict(foo='bar')
+        metadata.update(forbidden)
+        result = tools.clean_exr_metadadata(image, metadata)
+        for key in forbidden:
+            self.assertNotIn(key, result)
+
+        self.assertEqual(metadata['foo'], 'bar')
+
+    def test_write_exr(self):
+        with TemporaryDirectory() as root:
+            target = Path(root, 'test_exr.exr')
+            image = np.zeros((10, 5, 4), dtype=np.float16)
+            tools.write_exr(target, image, {})
+            self.assertTrue(os.path.exists(target))
+
+    def test_write_exr_two_channel(self):
+        with TemporaryDirectory() as root:
+            target = Path(root, 'test_exr.exr')
+            image = np.zeros((10, 5), dtype=np.float16)
+            tools.write_exr(target, image, {})
+            self.assertTrue(os.path.exists(target))
+
+    def test_write_exr_error(self):
+        with TemporaryDirectory() as root:
+            target = Path(root, 'test_exr.exr')
+            image = np.zeros((10, 5), dtype=np.uint8)
+
+            expected = 'EXR cannot be saved with array of dtype: uint8.'
+            with self.assertRaisesRegex(TypeError, expected):
+                tools.write_exr(target, image, {})
